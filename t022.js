8940eb76e99b8a11bc588e4c660254c246d27ea5
kakuilan <kakuilan@163.com>
2019-09-01 22:42:19 +0800

    es6 类的继承

--- t022.js
+++ t022.js
@@ -1,5 +1,6 @@
 // es6 class类
 
+// 定义类
 class Point {
   constructor (x, y) {
     this.x = x
@@ -11,5 +12,22 @@ class Point {
   }
 }
 
+// 继承类,使用extends
+class ColorPoint extends Point {
+  constructor (x, y, color) {
+    super(x, y) // 等同于super.constructor(x,y)
+    this.color = color
+  }
+
+  toString () {
+    return this.color + ' ' + super.toString()
+  }
+}
+
+// 基类
 var point = new Point(2, 3)
 console.log(point.toString())
+
+// 子类
+var child = new ColorPoint(4, 5, 'red')
+console.log(child.toString())
