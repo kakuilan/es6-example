af4b51c3c7693086ff125a9b227b5da9bf6d548a
kakuilan <kakuilan@163.com>
2019-09-03 21:11:13 +0800

    import as别名

--- index.js
+++ index.js
@@ -4,4 +4,8 @@
 // 导入-批量导出的变量
 import { firstName, lastName, year } from './t024'
 
+// 别名
+import { firstName as N1, lastName as N2, year as N3 } from './t023'
+
 console.log(firstName, lastName, year)
+console.log(N1, N2, N3)
