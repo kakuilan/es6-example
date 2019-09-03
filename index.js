c2c612e91ce766ffd5d87b8148aa1e8ce2fe8604
kakuilan <kakuilan@163.com>
2019-09-03 21:03:25 +0800

    es6 export 一起导出几个变量

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-// 引入单独导出的变量
-import { firstName, lastName, year } from './t023'
+// 引入-单独导出的变量
+// import { firstName, lastName, year } from './t023'
+
+// 导入-批量导出的变量
+import { firstName, lastName, year } from './t024'
+
 console.log(firstName, lastName, year)
---
+++ t024.js
@@ -0,0 +1,6 @@
+// es6 export 一起导出几个变量
+var firstName = 'David'
+var lastName = 'Belle'
+var year = 1973
+
+export { firstName, lastName, year }
